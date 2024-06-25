ce3c97be1801f2e5610c2fb1623921903d6d7519
cameronkc <cameronleekcmo@gmail.com>
2024-06-24 20:23:27 -0500

    netlify changes

--- functions/app.js
+++ functions/app.js
@@ -2,10 +2,20 @@ const express = require('express');
 const axios = require('axios');
 const dotenv = require('dotenv');
 const path = require('path');
+const serverless = require("serverless-http");
+const router = express.Router();
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+router.get("/", (req, res) => {
+    res.send("App is running..");
+});
+
+app.use("/.netlify/functions/app", router);
+module.exports.handler = serverless(app);
 
 const fetchSenateTrades = async () => {
   const apiUrl = `https://financialmodelingprep.com/api/v4/senate-trading-rss-feed?page=0&apikey=${process.env.API_KEY}`;
